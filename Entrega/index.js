f31ef8c45956928a38795ff0f5214c67d5c527cd
Yangmape <134799815+Mateuspsantos1@users.noreply.github.com>
2023-05-27 14:45:02 -0300

    Add files via upload
    Obs: caso o numero colocado na resposta da pergunta dor menor que 10 adicionar o zero na frente ( até tentei programar pra fazer isso mas falhei miseravelmente sjdklahsjd)

---
+++ Entrega/index.js
@@ -0,0 +1,8 @@
+const Dia = prompt("Dia de Nascimento")
+const Mês = prompt("Mês de Nascimento")
+const Ano = prompt("Ano de Nascimento")
+const nascimento =  Dia + "/" + Mês + "/" + Ano
+alert(nascimento)
+
+console.log(nascimento)
+
